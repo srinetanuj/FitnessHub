1a8f0f0cd6875a1cb1e67cc015b201ad3c46831a
arnabdasrishi <arnabdasrishi@gmail.com>
2022-12-14 20:37:11 +0530

    login-signup page -ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Login from './Components/Login/LoginPage';
+import SignupCard from './Components/Login/SignupPage';
 
 function App() {
   return (
     <div className="App">
-     
+     <SignupCard />
+     <Login />
     </div>
   );
 }
