c778d8b0cef8d8c3f55a7f113603e0c15d948a3d
pgariya <8192969855p@gmail.com>
2022-12-14 12:43:28 +0530

    day-1 work for construct week

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Blog from './Pages/Blog';
 
 function App() {
   return (
     <div className="App">
-     
+     <Blog/>
     </div>
   );
 }
---
+++ src/Pages/AllRoutes.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+function AllRoutes (){
+
+return(
+    <Routes>
+
+<Route></Route>
+
+
+
+    </Routes>
+)
+
+
+}
