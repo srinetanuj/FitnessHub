c38f5cd41ba979ecda8a8a14a2740d25c34575ea
Akhilesh yadav <akhileshyadav310596@gmail.com>
2022-12-15 21:54:17 +0530

    support

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { AllRoutes } from './Pages/AllRoutes';
 
 
+
 function App() {
   return (
 
     <div className="App">
 
-    
-     <AllRoutes/>
+   
+     {/* <AllRoutes/> */}
+     
 
 
     
