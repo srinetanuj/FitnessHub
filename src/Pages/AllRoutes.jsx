131a3da56bc8f50792aa6aa53b8a22b61eb7c7ed
Tinkle7 <tinkledash@gmail.com>
2022-12-15 06:35:49 +0530

    first commit

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Blog from "./Blog";
+import About from "./DatasPage/About";
 import AppUpdatePage from "./DatasPage/AppUpdatePage";
 import FeaturesPage from "./DatasPage/FeaturesPage";
 import FitnessPage from "./DatasPage/FitnessPage";
@@ -10,29 +11,20 @@ import RecipesPage from "./DatasPage/RecipesPage";
 import SearchPage from "./DatasPage/SearchPage";
 import TipsPage from "./DatasPage/TipsPage";
 
-export function AllRoutes (){
-
-return(
+export function AllRoutes() {
+  return (
     <Routes>
-
-<Route path="/blog" element={<Blog/>} />  
-<Route path="/blog/recentPost" element={<RecentPostPage/>} />  
-<Route path="/blog/nutrition" element={<NutritionPage/>} />  
-<Route path="/blog/fitness" element={<FitnessPage/>} />  
-<Route path="/blog/recipes" element={<RecipesPage/>} />  
-<Route path="/blog/appUpdate" element={<AppUpdatePage/>} />  
-<Route path="/blog/features" element={<FeaturesPage/>} /> 
-<Route path="/blog/tips" element={<TipsPage/>} />  
-<Route path="/blog/knowYourself" element={<KnowYourselfPage/>} />  
-<Route path="/blog/search" element={<SearchPage/>} />  
-
-
-
-
-
-
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/recentPost" element={<RecentPostPage />} />
+      <Route path="/blog/nutrition" element={<NutritionPage />} />
+      <Route path="/blog/fitness" element={<FitnessPage />} />
+      <Route path="/blog/recipes" element={<RecipesPage />} />
+      <Route path="/blog/appUpdate" element={<AppUpdatePage />} />
+      <Route path="/blog/features" element={<FeaturesPage />} />
+      <Route path="/blog/tips" element={<TipsPage />} />
+      <Route path="/blog/knowYourself" element={<KnowYourselfPage />} />
+      <Route path="/blog/search" element={<SearchPage />} />
+      <Route path="/About us" element={<About />} />
     </Routes>
-)
-
-
+  );
 }
